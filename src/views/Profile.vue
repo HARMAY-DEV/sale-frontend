9d00d3102b90867625fdec383ff870da5cefc3d9
haodongwang <haodongwang@tencent.com>
2021-06-01 23:15:12 +0800

    feat(profile): support logout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>个人中心</div>
+  <div>
+    <h1>个人中心</h1>
+    <el-button type="primary" @click="logout()">退出登录</el-button>
+  </div>
 </template>
 
 <script>
+import { Storage } from '@/utils/storage';
+
 export default {
   name: 'Profile',
+  methods: {
+    logout() {
+      Storage.clear();
+      location.reload();
+    },
+  },
 }
 </script>
 
