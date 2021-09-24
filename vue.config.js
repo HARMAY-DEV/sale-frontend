5f80d50fa7120f8219d5e0d860ed3ddae51196ec
hs <H17614304574@163.com>
2021-09-24 14:47:46 +0800

    接口更改

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,9 @@
 // }
 
 module.exports = {
-  publicPath: './',
-  devServer: {
+  // publicPath: './',
+ publicPath: './',
+   devServer: {
     open: true, // auto open brower 项目启动后自动打开浏览器...
     disableHostCheck: false,
     // host: "127.0.0.1",
@@ -13,7 +14,7 @@ module.exports = {
     https: false,
     hotOnly: false, // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
     proxy: {     // string | Object 解决跨域问题
-      '/api': {
+      '/api': { 
         target: 'http://172.100.21.86:8703', // 对应自己的 跨域地址 即请求的后端地址
         // target: 'http://172.100.49.208:8703', // 对应自己的 跨域地址 即请求的后端地址
         changeOrigin: true,
